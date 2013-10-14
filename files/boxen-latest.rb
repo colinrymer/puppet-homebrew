eee2db821bc1609210ce69fb07659ebcf05d6a2d
Colin Rymer <colin.rymer@gmail.com>
2013-10-14 13:28:18 -0400

    remove old require statement

--- files/boxen-latest.rb
+++ files/boxen-latest.rb
@@ -1,5 +1,3 @@
-require "boxen-monkeypatches"
-
 # A custom Homebrew command that gives us the latest available version
 # for a given formula. This allows us to avoid the terrifying prospect
 # of loading Homebrew Ruby code in the Puppet process.
